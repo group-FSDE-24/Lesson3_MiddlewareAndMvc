0ec585f9ae8f939345517a13afbee7b31a503c95
karimzade_k <karimzade_k@STHQ0118-01>
2026-01-16 21:33:01 +0400

    MVC Controller

---
+++ Lesson2_Middleware/Middlewares/Concretes/MVCMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Lesson2_Middleware.Middlewares.Abstracts;
+using Lesson3_MiddlewareAndMvc.Controllers.Abstract;
+using System.Net;
+using System.Reflection;
+
+namespace Lesson3_MiddlewareAndMvc.Middlewares.Concretes;
+
+public class MVCMiddleware : IMiddleware
+{
+    public HttpHandler? Next { get ; set ; }
+
+    public void Handler(HttpListenerContext httpListenerContext)
+    {
+        string? url = httpListenerContext.Request.Url.AbsolutePath;
+
+        var data = url.Split('/', StringSplitOptions.RemoveEmptyEntries);
+        var controllerName = $"Lesson3_MiddlewareAndMvc.Controllers.Concretes.{data[0]}Controller";
+
+        var assembly = Assembly.GetExecutingAssembly();
+
+        var type = assembly.GetType(controllerName);
+
+        if (type is not null)
+        {
+            var controolerObj = Activator.CreateInstance(type) as Controller;
+
+            if (controolerObj is not null)
+            {
+                var actionName = data[1];
+                var action = type.GetMethod(actionName);
+
+                if (action is not null)
+                {
+                    controolerObj.Context = httpListenerContext;
+
+                    action.Invoke(controolerObj, null);
+                }
+
+            }
+
+            Next?.Invoke(httpListenerContext);
+        }
+
+
+
+        throw new NotImplementedException();
+    }
+}
